7147d6ed9f5b977e51a6b2fba9d1ba6f1a945728
Formatted <Formatted@users.noreply.github.com>
2016-04-08 08:27:31 +0200

    Update script.js

--- Day4/script.js
+++ Day4/script.js
@@ -8,25 +8,31 @@ var cnt = 0;
 var string = "";
 n = prompt("Give me a number");
 
+var isEven = function(x) {
+    if (x % 2 === 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
+
 while (cnt < n) {
-  if (number % 2 == 0) {
-    	if (cnt === n - 1) {
-        string = string + "<p class=\"even\">" + number + "</p>" + "<hr>";
-	    } else {
-        string = string + "<p class=\"even\">" + number + "," + "</p>";
-	    }
-  } else {
-  	  if (cnt === n - 1) {
-        string = string + "<p class=\"odd\">" + number + "</p>" + "<hr>";
-	    } else {
-        string = string + "<p class=\"odd\">" + number + "," + "</p>";
-	    }
-
-  }
-
-
+    if (isEven(number)) {
+    if (cnt === n - 1) {
+        string = string + "<p class='even'>" + number + "</p>";
+    } else {
+        string = string + "<p class='even'>" + number + "," + "</p>" + "<hr>";
+    }
+    } else {
+     if (cnt === n - 1) {
+        string = string + "<p class='odd'>" + number + "</p>";
+    } else {
+        string = string + "<p class='odd'>" + number + "," + "</p>" + "<hr>";
+    }   
+    }
     cnt += 1;
     number += 1;
-}
+};
 
 document.querySelector('div').innerHTML = string;
+
